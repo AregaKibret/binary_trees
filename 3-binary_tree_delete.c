cda996a75e6c7d2072b81033bc6f4aafb41997cb
AregaKibret <agi1wise@gmail.com>
2024-03-29 09:17:29 -0700

    s

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 /**
  * binary_tree_delete - free an entire tree using recursion it free the node
- * return: Nothing
+ * when the node left and right are NULL
+ * @tree: tree to free
+ * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
